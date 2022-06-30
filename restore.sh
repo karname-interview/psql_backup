781beca015f50c63802e533b9d1d45ffd094208f
ArshiAAkhavan <letmemakenewone@gmail.com>
2022-06-30 21:27:13 +0430

    add comments for restore.sh

--- restore.sh
+++ restore.sh
@@ -5,7 +5,10 @@ USERNAME=$3
 DATABASE=$4
 BUCKET=$5
 
+
+# download backup file from S3 server
 mc alias set s3 $S3_DOMAIN $S3_ACCESS_KEY $S3_SECRET_KEY 1>&2
 mc cp s3/$BUCKET/$BACKUP_NAME ./$BACKUP_NAME 1>&2
 
+# restoring backup
 psql -h $DOMAIN -p $PORT -d $DATABASE -U $USERNAME <$BACKUP_NAME 
