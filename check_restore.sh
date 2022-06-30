e9678778e7a53c77ec06e2bfe67cc636f5a01aa3
ArshiAAkhavan <letmemakenewone@gmail.com>
2022-06-30 17:28:53 +0430

    better ui check_restore

--- check_restore.sh
+++ check_restore.sh
@@ -5,5 +5,6 @@ mc alias set s3 $S3_DOMAIN $S3_ACCESS_KEY $S3_SECRET_KEY 1>&2
 mc cp s3/$BUCKET/$BACKUP_NAME ./$BACKUP_NAME 1>&2
 
 # run postgres
-/opt/bitnami/scripts/postgresql/entrypoint.sh /opt/bitnami/scripts/postgresql/run.sh&
+/opt/bitnami/scripts/postgresql/entrypoint.sh /opt/bitnami/scripts/postgresql/run.sh 1>&2 2>/dev/null&
+echo "postgres server started"
 psql -h localhost -p 5432 -U postgres <$BACKUP_NAME 
