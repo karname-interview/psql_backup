7433b86ba9f6d4e1953cabcd1c14717b750f121b
ArshiAAkhavan <letmemakenewone@gmail.com>
2022-06-30 21:25:34 +0430

    add comments for backup.sh

--- backup.sh
+++ backup.sh
@@ -6,9 +6,11 @@ DATABASE=$4
 NAME=$5
 BUCKET=$6
 
+# create backup
 BACKUP_NAME=${NAME}_`date +"%m-%d-%y_%H-%M-%S_%s"`.sql
 pg_dump -h $DOMAIN -p $PORT -d $DATABASE -U $USERNAME > $BACKUP_NAME 
 
+# upload to S3 server
 mc alias set s3 $S3_DOMAIN $S3_ACCESS_KEY $S3_SECRET_KEY 1>&2
 mc mb s3/$BUCKET 1>&2
 mc cp $BACKUP_NAME s3/$BUCKET 1>&2
